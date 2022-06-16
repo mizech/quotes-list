47c6d6cbb8b72c23bb657d3fc3657d8efd67a275
michael.zech <michael.zech@hotmail.de>
2022-06-16 08:55:39 +0200

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Michael Zech on 16.06.22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
